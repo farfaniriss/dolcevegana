dada078e8b8a2ea147bf572d0497740445e3692b
farfaniriss <iris.farfan.c@gmail.com>
2020-02-19 00:27:06 -0500

    added animations on  blog posts

--- src/views/Blog/Blog.ts
+++ src/views/Blog/Blog.ts
@@ -16,16 +16,19 @@ export default class Blog extends Vue {
     axios
       .get(`https://picsum.photos/v2/list?page=${this.page}&limit=10`)
       .then(response => {
-        console.log(response.data);
         if (response.data.length > 0) {
           this.page += 1;
-          this.posts.push(response.data);
-          this.loading = false;
+          response.data.forEach((element: any) => {
+            element.url =
+              "https://image.freepik.com/foto-gratis/bocadillos-anacardos-tazon-ceramica-cucharas-madera_34056-458.jpg";
+          });
+          this.posts = response.data;
+          console.log(this.posts);
+          setTimeout(() => {
+            this.loading = false;
+          }, 1000);
         }
       });
-    // setTimeout(() => {
-    //   this.loading = false;
-    // }, 1000);
   }
 
   infiniteHandler($state: any) {
@@ -34,7 +37,11 @@ export default class Blog extends Vue {
       .then(response => {
         if (response.data.length > 0) {
           this.page += 1;
-          this.posts.push(response.data);
+          response.data.forEach((element: any) => {
+            element.url =
+              "https://image.freepik.com/foto-gratis/quinua-cruda-cerca-tazon-madera_136346-2133.jpg";
+            this.posts.push(element);
+          });
           $state.loaded();
         } else {
           $state.complete();
--- src/views/Blog/Blog.vue
+++ src/views/Blog/Blog.vue
@@ -1,19 +1,39 @@
 <template>
-  <v-layout row wrap>
-    <!-- <v-flex xs12 sm12>
-      <v-skeleton-loader :loading="loading" transition="fade-transition" height="94" type="image">
-        <v-card>
-          <v-card-title>Title</v-card-title>
-          <v-card-text>Card Text</v-card-text>
-        </v-card>
-      </v-skeleton-loader>
-    </v-flex>-->
-    <div v-if="!loading">
-      <v-flex xs2 sm4 v-for="(post, $index) in posts" :key="$index">
-        <v-img :src="post.download_url + '.jpg'" aspect-ratio="2"></v-img>
-      </v-flex>
-      <infinite-loading @infinite="infiniteHandler"></infinite-loading>
-    </div>
-  </v-layout>
+  <v-container>
+    <v-row no-gutters>
+      <v-col v-for="(post, index) in posts" :key="index" cols="12" sm="4">
+        <v-skeleton-loader
+          :loading="loading"
+          transition="fade-transition"
+          type="image"
+          height="417"
+        >
+          <v-hover v-slot:default="{ hover }">
+            <v-img id="img" :src="post.url" :class="{ 'on-hover': hover }">
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-fast-in-fast-out gray v-card--reveal display-2 font-weight-bold white--text text--lighten-5"
+                  style="height: 100%;"
+                >
+                  Cashews
+                </div>
+              </v-expand-transition>
+            </v-img>
+          </v-hover>
+        </v-skeleton-loader>
+      </v-col>
+    </v-row>
+    <infinite-loading @infinite="infiniteHandler"></infinite-loading>
+  </v-container>
 </template>
-<script lang="ts" src="./Blog.ts">
+<script lang="ts" src="./Blog.ts"></script>
+<style scoped>
+#img {
+  transition: opacity 0.4s ease-in-out;
+}
+
+#img:not(.on-hover) {
+  opacity: 0.6;
+}
+</style>
